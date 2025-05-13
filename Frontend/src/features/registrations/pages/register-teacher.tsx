ff46580ff51b84530886c3158e44176709327f03
Ivan Herlan Herbas Zubieta <79385821+ivancidev@users.noreply.github.com>
2025-05-13 14:42:39 -0400

    feat: add teacher registration form and page components

---
+++ Frontend/src/features/registrations/pages/register-teacher.tsx
@@ -0,0 +1,9 @@
+import { FormRegisterTeacher } from '../components/form-register-teacher';
+
+export default function RegisterTeacherPage() {
+  return (
+    <div className="h-full w-full flex flex-col items-center justify-center">
+      <FormRegisterTeacher />
+    </div>
+  );
+}
