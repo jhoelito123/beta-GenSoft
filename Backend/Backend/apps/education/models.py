dce1ec3a238ee9aa968b62d6959ba3edaca3d716
Adam Jhoel <amercer732@gmail.com>
2025-05-13 01:49:34 -0400

    Fix: rename Municipio -> Provincia

--- Backend/Backend/apps/education/models.py
+++ Backend/Backend/apps/education/models.py
@@ -8,13 +8,13 @@ class Departamento(models.Model):
     def __str__(self):
         return self.nombre_departamento
 
-class Municipio(models.Model):
-    id_municipio = models.AutoField(primary_key=True)
-    nombre_municipio = models.CharField(max_length=150)
+class Provincia(models.Model):
+    id_provincia = models.AutoField(primary_key=True)
+    nombre_provincia = models.CharField(max_length=150)
     departamento = models.ForeignKey(Departamento, on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.nombre_municipio
+        return self.nombre_provincia
     
 class NivelEducativo(models.Model):
     id_nivel_educativo = models.AutoField(primary_key=True)
@@ -30,7 +30,7 @@ class Institucion(models.Model):
     codigo_institucion = models.CharField(max_length=10, unique=True)
     direccion = models.CharField(max_length=50)
     telefono = models.BigIntegerField()
-    municipio = models.ForeignKey(Municipio, on_delete=models.CASCADE)
+    provincia = models.ForeignKey(Provincia, on_delete=models.CASCADE)
 
     class Meta:
         verbose_name = "Institución"
