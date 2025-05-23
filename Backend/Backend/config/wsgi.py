ac8c07442ab24a05dc2b77512d2e9f56ab58fe36
Adam Jhoel <amercer732@gmail.com>
2025-05-23 02:23:49 -0400

    Build: ultimate attemp for deploy

--- Backend/Backend/config/wsgi.py
+++ Backend/Backend/config/wsgi.py
@@ -1,8 +1,12 @@
-
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings.base')
+PROJECT_DJANGO_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..'))
+if PROJECT_DJANGO_ROOT not in sys.path:
+    sys.path.insert(0, PROJECT_DJANGO_ROOT)
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Backend.config.settings.base')
 
 application = get_wsgi_application()
