898ff0a6e7e60c00900f10490295cfb15c3b08db
Adam Jhoel <amercer732@gmail.com>
2025-05-23 00:47:08 -0400

    build: changue build.sh for render deploy

--- Backend/Backend/config/settings/base.py
+++ Backend/Backend/config/settings/base.py
@@ -4,8 +4,6 @@ import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
-#print(f"BASE_DIR is: {BASE_DIR}") # Imprime el valor de BASE_DIR
-
 
 # Quick-start development settings - unsuitable for production
 
--- Backend/build.sh
+++ Backend/build.sh
@@ -3,10 +3,10 @@
 set -o errexit
 
 # Modify this line as needed for your package manager (pip, poetry, etc.)
-pip install -r requirements.txt
+pip install -r ./Backend/requirements.txt
 
 # Convert static asset files
-python ./Backend/manage.py collectstatic --no-input
+python ./Backend/Backend/manage.py collectstatic --no-input
 
 # Apply any outstanding database migrations
-python ./Backend/manage.py migrate
+python ./Backend/Backend/manage.py migrate
