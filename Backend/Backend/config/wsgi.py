f931874a928ff34cd5aa72de6752912bd533eb45
Adam Jhoel <amercer732@gmail.com>
2025-05-23 10:51:03 -0400

    Build: changue wsgi application

--- Backend/Backend/config/wsgi.py
+++ Backend/Backend/config/wsgi.py
@@ -3,10 +3,6 @@ import sys
 
 from django.core.wsgi import get_wsgi_application
 
-PROJECT_DJANGO_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..'))
-if PROJECT_DJANGO_ROOT not in sys.path:
-    sys.path.insert(0, PROJECT_DJANGO_ROOT)
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Backend.config.settings.base')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings.base')
 
 application = get_wsgi_application()
