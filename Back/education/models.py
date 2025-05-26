7f87a930891bad9529bb87b1b22f1b1077efbb69
Adam Jhoel <amercer732@gmail.com>
2025-05-26 12:00:57 -0400

    Feat: create models Course and Section

--- Back/education/models.py
+++ Back/education/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from users.models import Admin
+from users.models import Admin, Docente
 
 class Departamento(models.Model):
     id_departamento = models.AutoField(primary_key=True)
@@ -41,3 +41,50 @@ class Institucion(models.Model):
 
     def __str__(self):
         return self.nombre_institucion
+
+class Modulo(models.Model):
+    id_modulo = models.AutoField(primary_key=True)
+    nombre_modulo = models.CharField(max_length=40)
+    
+    def __str__(self):
+        return self.nombre_modulo
+
+class Idioma(models.Model):
+    id_idioma = models.AutoField(primary_key=True)
+    idioma = models.CharField(max_length=40)
+    
+    def __str__(self):
+        return self.idioma
+
+class DificultadCurso(models.Model):
+    id_dificultad = models.AutoField(primary_key=True)
+    dificultad_curso = models.CharField(max_length=40)
+    
+    def __str__(self):
+        return self.dificultad_curso
+
+class Curso(models.Model):
+    id_curso = models.AutoField(primary_key=True)
+    nombre_curso = models.CharField(max_length=30)
+    profesor_curso = models.ForeignKey(Docente, on_delete=models.CASCADE)
+    calificacion_curso = models.FloatField()
+    duracion_curso = models.TimeField()
+    descripcion_curso = models.TextField()
+    portada_curso = models.URLField()
+    fecha_inicio_curso = models.DateField()
+    fecha_cierre_curso = models.DateField()
+    modulo_curso = models.ForeignKey(Modulo, on_delete=models.CASCADE)
+    idioma_curso = models.ForeignKey(Idioma, on_delete=models.CASCADE)
+    dificultad_curso = models.ForeignKey(DificultadCurso, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.nombre_curso
+
+class Seccion(models.Model):
+    id_seccion = models.AutoField(primary_key=True)
+    nombre_seccion = models.CharField(max_length=30)
+    descripcion_seccion = models.TextField()
+    seccion_del_curso = models.ForeignKey(Curso, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.nombre_seccion
