cb00e8ca1ca83bfd1e4080c55b1ecb8aeeec8f72
AndreaQuelali <andrealizbethquelali5@gmail.com>
2025-05-27 17:52:39 -0400

    feat:  add icon svg in .tsx with currentColor for card course

---
+++ Frontend/src/features/courses/icons/code.tsx
@@ -0,0 +1,19 @@
+export default function IconCode() {
+  return (
+    <svg
+      width="20"
+      height="20"
+      viewBox="0 0 20 20"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M13.3333 15L18.3333 10L13.3333 5M6.66663 5L1.66663 10L6.66663 15"
+        stroke="currentColor"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  );
+}
