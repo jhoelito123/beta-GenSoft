50fcf98804a270629318c941b86b72fb487e21e6
David Huanca Ledezma <81874354+DavidHuancaLedezma@users.noreply.github.com>
2025-06-01 23:58:09 -0400

    feat: add test create user

--- Backend/users/tests.py
+++ Backend/users/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from users.models import Usuario
 
-# Create your tests here.
+class UsuarioModelTest(TestCase):
+    def test_creacion_usuario(self):
+        
+        usuario = Usuario.objects.create(
+            username_user="lucas",
+            password_user="12345678",
+            email_user="lucas@gmail.com"
+        )
+
+        self.assertEqual(usuario.username_user, "lucas")
+        self.assertEqual(usuario.email_user, "lucas@gmail.com")
+        self.assertTrue(usuario.is_active)
