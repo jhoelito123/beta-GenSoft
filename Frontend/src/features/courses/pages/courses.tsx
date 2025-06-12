2261565590ce200b74d63afd36c22ecab8107db4
AndreaQuelali <andrealizbethquelali5@gmail.com>
2025-06-12 12:10:02 -0400

    feat: create component search and implement in coruses

--- Frontend/src/features/courses/pages/courses.tsx
+++ Frontend/src/features/courses/pages/courses.tsx
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+import SearchBar from '../../../components/ui/search';
 import { API_URL } from '../../../config/api-config';
 import { useFetchData } from '../../../hooks/use-fetch-data';
 import CardCourse from '../components/card-course';
@@ -18,37 +20,51 @@ type Curso = {
 };
 
 export default function CoursesPage() {
+  const [query, setQuery] = useState('');
   const {
     data: cursos,
     loading,
     error,
-  } = useFetchData<Curso[]>(`${API_URL}/education/curso/`);
+  } = useFetchData<Curso[]>(`${API_URL}/education/curso`);
+
+  const cursosFiltrados = cursos?.filter((curso) =>
+    curso.nombre_curso.toLowerCase().includes(query.toLowerCase()),
+  );
 
   if (loading) return <p className="p-4">Cargando cursos...</p>;
   if (error) return <p className="p-4 text-red-500">Error al cargar cursos</p>;
 
   return (
     <div className="p-8">
-      <h1 className="text-3xl font-bold mb-6 text-slate-900">
-        Cursos Disponibles
-      </h1>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
-        {cursos?.map((curso) => (
-          <CardCourse
-            key={curso.id_curso}
-            level={mapDificultad(curso.dificultad_curso)}
-            image={curso.portada_curso}
-            title={curso.nombre_curso}
-            college="Universidad Andina"
-            rating={curso.calificacion_curso}
-            votes={0}
-            sections={1}
-            practices={2}
-            quizzez={3}
-            link={`/show-course/${curso.id_curso}`}
-          />
-        ))}
+      <div className="flex justify-between items-center mb-6">
+        <h1 className="text-3xl font-bold text-slate-900">
+          Cursos Disponibles
+        </h1>
+        <SearchBar query={query} onChangeQuery={setQuery} />
       </div>
+      {cursosFiltrados?.length === 0 ? (
+        <p className="text-neutral-500 mt-56 w-full flex items-center justify-center body-lg">
+          No se encontraron cursos.
+        </p>
+      ) : (
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
+          {cursosFiltrados?.map((curso) => (
+            <CardCourse
+              key={curso.id_curso}
+              level={mapDificultad(curso.dificultad_curso)}
+              image={curso.portada_curso}
+              title={curso.nombre_curso}
+              college="Universidad Andina"
+              rating={curso.calificacion_curso}
+              votes={0}
+              sections={1}
+              practices={2}
+              quizzez={3}
+              link={`/show-course/${curso.id_curso}`}
+            />
+          ))}
+        </div>
+      )}
     </div>
   );
 }
