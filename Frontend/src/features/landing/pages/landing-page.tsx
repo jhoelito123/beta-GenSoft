1a9fc6241ffd7a00bdee32194df59aede5dcb8fd
Adam Jhoel <amercer732@gmail.com>
2025-06-13 17:34:16 -0400

    Style: landing with levels education

--- Frontend/src/features/landing/pages/landing-page.tsx
+++ Frontend/src/features/landing/pages/landing-page.tsx
@@ -1,6 +1,7 @@
 import Footer from "../../../components/ui/footer";
 import Blog from "../components/Blog";
 import Hero from "../components/hero";
+import EducationLevelButtons from "../components/institucionals";
 import Pricing from "../components/Pricing";
 import Testimonials from "../components/Testimonials";
 
@@ -8,6 +9,7 @@ const MainLayout = () => {
   return (
       <div className="bg-[#FCFCFC] w-full flex flex-col items-center justify-center">
         <Hero/>
+        <EducationLevelButtons/>
         <Testimonials/>
         <Pricing/>
         <Blog/>
